9468f99f308ace0d6a35f0dd3b5389d5d9d4fa68
Josué Ttito <josue.ttito@ucsp.edu.pe>
2019-01-12 12:07:48 -0500

    Fix cmake

--- code/planecalib/CeresUtils.h
+++ code/planecalib/CeresUtils.h
@@ -1,8 +1,12 @@
 /*
- * ceresutils.h
+ * CeresUtils.h
  *
- *  Created on: 25.2.2014
- *      Author: dan
+ * Copyright(C) 2014, University of Oulu, all rights reserved.
+ * Copyright(C) 2014, NVIDIA Corporation, all rights reserved.
+ * Third party copyrights are property of their respective owners.
+ * Contact: Daniel Herrera C. (dherrera@ee.oulu.fi),
+ *          Kihwan Kim(kihwank@nvidia.com)
+ * Author : Daniel Herrera C.
  */
 
 #ifndef CERESUTILS_H_
